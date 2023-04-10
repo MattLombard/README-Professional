d0a022c55d27b59a1c78f2aa2f7be5da433a3363
Matt Lombard <ncmattlombard@gmail.com>
2023-04-10 01:08:33 -0400

    recreated the folder structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
@@ -54,7 +54,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-let fileName = 'userREADME.md';
+let fileName = './example/userREADME.md';
 
 function writeToFile(fileName, data) {
   fs.appendFile(fileName, data, (err) => {
